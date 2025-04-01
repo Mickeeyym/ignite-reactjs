d0714dc689a7b436b736a6ae4e53fc0a28d7571f
Michelangelo Morais <mickeey.m.200@gmail.com>
2025-04-01 16:34:17 -0300

    Componentes Criados e Começo da Estilização

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { Header } from "./components/Header";
+import {Post} from "./Post";
+
+import styles from './App.module.css';
+
+import './global.css';
+import { Sidebar } from "./components/Sidebar";
+
+export function App() {
+  return (
+    <div>
+      <Header></Header>
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+        <Post 
+        author = "Mickey" 
+        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi voluptatibus, natus dolore consectetur temporibus praesentium ex, adipisci soluta, alias a molestiae? Sint molestias impedit quis magnam possimus odit doloremque sed?"
+        />
+        </main>
+      </div>
+      
+    </div>
+  )
+}
+
+
