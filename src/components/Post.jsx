ae592ed8442a353dba699460cc99877773008bdf
Michelangelo Morais <mickeey.m.200@gmail.com>
2025-04-02 16:16:53 -0300

    Componente Post criado

---
+++ src/components/Post.jsx
@@ -0,0 +1,27 @@
+import styles from './Post.module.css';
+
+export function Post(props) {
+    return (
+        <article className={styles.post}>
+            <header>
+                <div className={styles.author}>
+                    <img className={styles.avatar} src="https://media.licdn.com/dms/image/v2/D4D03AQGZdfbmORJSFw/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1726497455491?e=1749081600&v=beta&t=pUyvt3pWCpY1vp8ywVPQQazluSbgcVGHs6Rk5Rk-Dj8"  />
+                    <div className={styles.authorInfo}>
+                        <strong>Michelangelo Morais</strong>
+                        <span>Web Developer</span>
+                    </div>
+                </div>
+
+                <time title='02 de abril às 16:00h' datetime="2025-04-02 16:00:00"> Publicado há 1h</time>            
+            </header>
+
+            <div className={styles.content}>
+                <p>Fala galeraa 👋</p>
+                <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+                <p>👉 <a href="">jane.design/doctorcare</a></p>
+                <p> <a href="">#novoprojeto #nlw #rocketseat</a></p>
+            </div>
+        </article>
+
+    )
+}
